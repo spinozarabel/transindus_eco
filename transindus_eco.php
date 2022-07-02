a7ddcde26634a7a89fe96b148e063978ab477764
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2022-07-02 17:08:51 +0530

    corrected error in action hook for cron

--- transindus_eco.php
+++ transindus_eco.php
@@ -29,7 +29,7 @@ add_action('plugins_loaded', 'this_plugin_init');
 
 add_filter( 'cron_schedules',  'shelly_studer_add_new_cron_interval' );
 
-if (!wp_next_scheduled('shelly_studer_task_hook')) 
+if (!wp_next_scheduled('shellystuder_task_hook')) 
 {
     wp_schedule_event( time(), 'sixty_seconds', 'shellystuder_task_hook' );
 }

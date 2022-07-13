a8c50c216003aff23f895b7ea8ced7bd32bfa315
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2022-07-13 14:32:32 +0530

    spaced quantities

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -655,11 +655,11 @@ class class_transindus_eco
                     </span>
                 </td>
             </tr>
-                <td id="grid_pin_ac_kw">' . $grid_pin_ac_kw . 'KW<br>' . $grid_input_vac . 'V</td>
+                <td id="grid_pin_ac_kw">' . $grid_pin_ac_kw . ' KW<br>' . $grid_input_vac . ' V</td>
                 <td id="grid_arrow_icon">' . $grid_arrow_icon  . '</td>
                 <td></td>
                 <td id="pv_arrow_icon">' . $pv_arrow_icon    . '</td>
-                <td id="psolar_kw">' . $psolar_kw . 'KW<br>' . $solar_pv_adc . 'A</td>
+                <td id="psolar_kw">' . $psolar_kw . ' KW<br>' . $solar_pv_adc . ' A</td>
 
             <tr>
                 <td></td>

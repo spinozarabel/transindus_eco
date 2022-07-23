35ba6de079578117300c4f24fdaf5660e2cf8270
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2022-07-23 21:07:30 +0530

    studer icon retweet

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -1927,7 +1927,7 @@ class class_transindus_eco
         $format_object->psolar_info   = $psolar_info;
 
         // Studer Inverter icon
-        $studer_icon = '<i class="fa-solid fa-3x fa-hard-drive"></i>';
+        $studer_icon = '<i class="fa-solid fa-3x fa-retweet"></i>';
         $format_object->studer_icon = $studer_icon;
 
         // battery status icon: select battery icon based on charge level

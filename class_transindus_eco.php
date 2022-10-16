aaa78f156e9f4dde4171a284fb86626c9e7d37f8
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2022-10-16 09:06:37 +0530

    error log rearrange for readability

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -373,7 +373,25 @@ class class_transindus_eco
         $KWH_load_today       = $studer_readings_obj->KWH_load_today;
         $KWH_batt_discharged_today = $studer_readings_obj->KWH_batt_discharged_today;
 
-        
+        if (true)
+        {
+            error_log("username: "             . $wp_user_name                             . "");
+            error_log("Shelly Switch State: "  . $shelly_switch_status                     . "");
+            error_log("Shelly Switch Servo: "  . $control_shelly                     . "");
+            error_log("Battery Avg Voltage: "  . $battery_voltage_avg                         . "Vdc ");
+            error_log("Battery Current: "      . $studer_readings_obj->battery_charge_adc     . "Adc ");
+
+            error_log("Solar PowerGen: "       . $psolar                                   . "KW ");
+            error_log("AC at Studer Input: "   . $shelly_api_device_status_voltage      	 . "Vac ");
+            error_log("Surplus PowerOut: "     . $surplus                                  . "KW ");
+            error_log("Calc Solar Pwr: "       . array_sum($est_solar_kw)                  . "KW ");
+            error_log("Cloudy Day?: "          . $it_is_a_cloudy_day                       . "");
+            error_log("Within 0700 - 1730?: "  . $now_is_daytime                           . "");
+            error_log("AUX1 Relay State: "     . $aux1_relay_state                         . "");
+            error_log("Solar Units Today: "    . $KWH_solar_today                          . "KWH");
+            error_log("Grid Units Today: "     . $KWH_grid_today                           . "KWH");
+            error_log("Load Units Today: "     . $KWH_load_today                           . "KWH");
+        }
 
         // get the SOC% from the previous reading from user meta
         $SOC_percentage_previous = get_user_meta($wp_user_ID, "soc_percentage_now",  true) ?? 50.0;
@@ -394,9 +412,9 @@ class class_transindus_eco
           // update the SOC percentage based on actuals. The update is algebraic. It can add or subtract
           // If there is no battery charging oby Grid Charge is Solar - discharge
           // $KWH_batt_charge_today    = $KWH_solar_today - $KWH_batt_discharged_today;
-          $KWH_batt_charge_today = $KWH_solar_today + ($KWH_grid_today - $KWH_load_today) * 1.04;
-          $SOC_KWH_now        = $SOC_KWH_beg_of_day + $KWH_batt_charge_today;
-          $SOC_percentage_now = round($SOC_KWH_now / $SOC_capacity * 100,1);
+          $KWH_batt_charge_today  = $KWH_solar_today + ($KWH_grid_today - $KWH_load_today) * 1.04;
+          $SOC_KWH_now            = $SOC_KWH_beg_of_day + $KWH_batt_charge_today;
+          $SOC_percentage_now     = round($SOC_KWH_now / $SOC_capacity * 100,1);
 
           $studer_readings_obj->SOC_percentage_now = $SOC_percentage_now;
 
@@ -407,29 +425,14 @@ class class_transindus_eco
             error_log("Battery discharge Units Today: "  . $KWH_batt_discharged_today      . "KWH");
             error_log("Battery Charge Units Today: "     . $KWH_batt_charge_today          . "KWH");
             error_log("SOC Percentage: "     . $SOC_percentage_now                         . "%");
-            error_log("");
+            error_log("");  // print out blank line for better readability
           }
         }
         
 
         if (true)
         {
-            error_log("user: "                 . $wp_user_name                             . ": Shelly and Studer Values");
-            error_log("Shelly Switch State: "  . $shelly_switch_status                     . "");
-            error_log("Shelly Switch Servo: "  . $control_shelly                     . "");
-            error_log("Battery Avg Voltage: "  . $battery_voltage_avg                         . "Vdc ");
-            error_log("Battery Current: "      . $studer_readings_obj->battery_charge_adc     . "Adc ");
-
-            error_log("Solar PowerGen: "       . $psolar                                   . "KW ");
-            error_log("AC at Studer Input: "   . $shelly_api_device_status_voltage      	 . "Vac ");
-            error_log("Surplus PowerOut: "     . $surplus                                  . "KW ");
-            error_log("Calc Solar Pwr: "       . array_sum($est_solar_kw)                  . "KW ");
-            error_log("Cloudy Day?: "          . $it_is_a_cloudy_day                       . "");
-            error_log("Within 0700 - 1730?: "  . $now_is_daytime                           . "");
-            error_log("AUX1 Relay State: "     . $aux1_relay_state                         . "");
-            error_log("Solar Units Today: "    . $KWH_solar_today                          . "KWH");
-            error_log("Grid Units Today: "     . $KWH_grid_today                           . "KWH");
-            error_log("Load Units Today: "     . $KWH_load_today                           . "KWH");
+            
             
 
         }

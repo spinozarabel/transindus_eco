ecc73b089eff0f8e708f090089c6e157f0be4a1f
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2022-10-21 08:59:55 +0530

    minor changes in conditions

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -461,7 +461,7 @@ class class_transindus_eco
         // On CLoudy date, LVDS trips at higher values of SOC% on normal days, it can go down lower before tripping
         // $LVDS_cloudy_day =  (   $it_is_a_cloudy_day && ( $battery_voltage_avg  <=  48.5 || $SOC_percentage_now <= 40 ) );
         // $LVDS_normal_day =  ( ! $it_is_a_cloudy_day && ( $battery_voltage_avg  <=  48.3 || $SOC_percentage_now <= 30 ) );      
-        $LVDS =             ( $battery_voltage_avg  <=  48.5 || $SOC_percentage_now <= 35 )  &&  // SOC is low
+        $LVDS =             ( $battery_voltage_avg  <=  48.3 || $SOC_percentage_now <= 30 )  &&  // SOC is low
                             ( $shelly_switch_status == "OFF" );					  // The switch is OFF
 
         $keep_switch_closed_always =  ( $shelly_switch_status == "OFF" )             &&
@@ -478,7 +478,7 @@ class class_transindus_eco
                                           ( $surplus <= -0.5 ) 														&&  // Solar Deficit >= 0.5KW
                                           ( $control_shelly == true );                        // Control Flag is SET
 
-        $switch_release =  (	$SOC_percentage_now >= 40  )                                &&  // SOC OK
+        $switch_release =  (	$SOC_percentage_now >= 35  )                                &&  // SOC OK
                            ( $shelly_switch_status == "ON" )  														&&  // Switch is ON now
                            ( $surplus >= 0.2 )                														&&  // Solar surplus is >= 0.2KW
                            ( $keep_shelly_switch_closed_always == false )                 &&	// Emergency flag is False

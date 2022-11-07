c022be4721ac55e7867e0906ddf585668ad49f2d
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2022-11-07 08:30:47 +0530

    changed weights for soc calc.

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -449,7 +449,7 @@ class class_transindus_eco
           $SOC_percentage_now_old = $SOC_percentage_beg_of_day + $SOC_batt_charge_net_percent_today_old;
 
           // This is the new simpler method. 
-          $SOC_batt_charge_net_percent_today = 0.93 * $KWH_solar_percentage_today - $KWH_batt_percent_discharged_today * 1.05;
+          $SOC_batt_charge_net_percent_today = 0.92 * $KWH_solar_percentage_today - $KWH_batt_percent_discharged_today * 1.00;
 
           $SOC_percentage_now = round($SOC_percentage_beg_of_day + $SOC_batt_charge_net_percent_today,1);
 

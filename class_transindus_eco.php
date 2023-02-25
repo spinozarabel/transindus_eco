5d5e317ee388baea1c1f339ba34875e1ccde289a
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2023-02-25 11:47:29 +0530

    sdfasdf

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -149,7 +149,7 @@ class class_transindus_eco
       $this->set_default_timezone();
 
       // set the logging
-      $this->verbose = false;
+      $this->verbose = true;
 
       // lat and lon at Trans Indus from Google Maps
       $this->lat        = 12.83463;

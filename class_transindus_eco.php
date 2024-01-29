53073db5faba60d7874679ba0159d4d62e67c85d
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2024-01-29 10:12:04 +0530

    dev

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -3512,12 +3512,12 @@ class class_transindus_eco
         $do_shelly  = get_user_meta($wp_user_ID, "do_shelly", true) ?? false;
 
         //
-        $readings_obj  = $this->get_readings_and_servo_grid_switch($user_index, $wp_user_ID, $wp_user_name, $do_shelly, false);
+        $shelly_readings_obj  = get_transient( "shelly_readings_obj" );
 
         $it_is_still_dark = $this->nowIsWithinTimeLimits( "18:55", "23:59:59" ) || $this->nowIsWithinTimeLimits( "00:00", "06:30" );
 
         // check for valid studer values. Return if empty.
-        if( empty(  $readings_obj ) )
+        if( empty(  $shelly_readings_obj ) )
         {
             if ( $it_is_still_dark )
             {
@@ -3532,7 +3532,7 @@ class class_transindus_eco
         }
 
         // get the format of all the information for the table in the page
-        $format_object = $this->prepare_data_for_mysolar_update( $wp_user_ID, $wp_user_name, $readings_obj );
+        $format_object = $this->prepare_data_for_mysolar_update( $wp_user_ID, $wp_user_name, $shelly_readings_obj );
 
         // define all the icon styles and colors based on STuder and Switch values
         $output .= '

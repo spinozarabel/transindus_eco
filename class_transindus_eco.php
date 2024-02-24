b2b4090eeeb01a7d6a98cef29814db699568d142
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2024-02-24 10:45:28 +0530

    properties onto shelly object

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -2515,34 +2515,43 @@ class class_transindus_eco
             }
           }
 
-          { // make an API call on studer using xcomlan shell exec script methid
-            $studer_data_via_xcomlan = get_transient("studer_data_via_xcomlan");
-
-            if ( ! empty( $studer_data_via_xcomlan ) )
+          { // Get studer data using xcomlan. A CRON MQTT on localhost publisher and a WP CRON MQTT subscriber
+            if ( false !== ( $studer_data_via_xcomlan = get_transient("studer_data_via_xcomlan") ) )
             {
-              $raw_batt_voltage_xcomlan = $studer_data_via_xcomlan->battery_voltage_xtender;
+              // transient data from cron MQTT is still available, so fresh since a maximum of 3 minutes
+              if ( ! empty( $studer_data_via_xcomlan ) && $studer_data_via_xcomlan->battery_voltage_xtender > 46.0 )
+              { // we seem to have valid data
+                $raw_batt_voltage_xcomlan = $studer_data_via_xcomlan->battery_voltage_xtender;
 
-              $east_panel_current = round( $studer_data_via_xcomlan->pv_current_now_1, 1 );
+                $east_panel_current_xcomlan       = round( $studer_data_via_xcomlan->pv_current_now_1, 1 );
 
-              $west_panel_current = round( $studer_data_via_xcomlan->pv_current_now_2, 1 );
+                $west_panel_current_xcomlan       = round( $studer_data_via_xcomlan->pv_current_now_2, 1 );
 
-              $pv_current_now_total = round( $studer_data_via_xcomlan->pv_current_now_total, 1 );
+                $pv_current_now_total_xcomlan     = round( $studer_data_via_xcomlan->pv_current_now_total, 1 );
 
-              $inverter_current = round( $studer_data_via_xcomlan->inverter_current, 1);
+                $inverter_current_xcomlan         = round( $studer_data_via_xcomlan->inverter_current, 1);
 
-              // battery current as measured by xcom-lan is got by adding + PV DC current amps and - inverter DC current amps
-              $batt_current_xcomlan = $pv_current_now_total + $inverter_current;
+                // battery current as measured by xcom-lan is got by adding + PV DC current amps and - inverter DC current amps
+                $batt_current_xcomlan = $pv_current_now_total_xcomlan + $inverter_current_xcomlan;
 
-              // calculate the voltage drop due to the battery current taking into account the polarity. + current is charging
-              // $battery_voltage_vdc = round($battery_voltage_vdc + abs( $inverter_current_amps ) * $Ra - abs( $battery_charge_amps ) * $Rb, 2);
+                // calculate the voltage drop due to the battery current taking into account the polarity. + current is charging
+                // $battery_voltage_vdc = round($battery_voltage_vdc + abs( $inverter_current_amps ) * $Ra - abs( $battery_charge_amps ) * $Rb, 2);
 
-              // if battery is charging voltage will decrease and if discharging voltage will increase due to IR compensation
-              $ir_drop_compensated_battery_voltage_xcomlan = $raw_batt_voltage_xcomlan - 0.025 * $batt_current_xcomlan;
+                // if battery is charging voltage will decrease and if discharging voltage will increase due to IR compensation
+                $ir_drop_compensated_battery_voltage_xcomlan = $raw_batt_voltage_xcomlan - 0.025 * $batt_current_xcomlan;
 
-              // calculate the running average over the last 5 readings including this one. Return is rounded to 2 decimals
-              $batt_voltage_xcomlan_avg = $this->get_battery_voltage_avg( $ir_drop_compensated_battery_voltage_xcomlan );
+                // calculate the running average over the last 5 readings including this one. Return is rounded to 2 decimals
+                $batt_voltage_xcomlan_avg = $this->get_battery_voltage_avg( $ir_drop_compensated_battery_voltage_xcomlan );
+
+                // pack these as properties onto the shelly readings object
+                $shelly_readings_obj->batt_voltage_xcomlan_avg          = $batt_voltage_xcomlan_avg;
+                $shelly_readings_obj->east_panel_current_xcomlan        = $east_panel_current_xcomlan;
+                $shelly_readings_obj->west_panel_current_xcomlan        = $west_panel_current_xcomlan;
+                $shelly_readings_obj->pv_current_now_total_xcomlan      = $pv_current_now_total_xcomlan;
+                $shelly_readings_obj->inverter_current_xcomlan          = $inverter_current_xcomlan;
+                $shelly_readings_obj->batt_current_xcomlan              = $batt_current_xcomlan;
+              }
             }
-            
           }
         }
 
@@ -2900,8 +2909,8 @@ class class_transindus_eco
           $log_string .= " SOC: $soc_percentage_now_display";
 
           $log_string = "Log -";
-          $log_string .= " E-Panel: $east_panel_current W-Panel: $west_panel_current";
-          $log_string .= " PV-Amps: $pv_current_now_total Inverter DC: $inverter_current";
+          $log_string .= " E-Panel: $east_panel_current_xcomlan W-Panel: $west_panel_current_xcomlan";
+          $log_string .= " PV-Amps: $pv_current_now_total_xcomlan Inverter DC: $inverter_current_xcomlan";
           $log_string .= " StdrBatt-Amps: $batt_current_xcomlan";
           $log_string .= " Shelly DC: $battery_amps BattV: $batt_voltage_xcomlan_avg";
 

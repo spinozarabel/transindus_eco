2a87b39a706fc27e7fc03044b3e2eb0f0efb6d79
spinozarabel <16438408+spinozarabel@users.noreply.github.com>
2024-03-04 10:44:00 +0530

    dev

--- class_transindus_eco.php
+++ class_transindus_eco.php
@@ -859,19 +859,8 @@ class class_transindus_eco
         }
 
         // The measure ADC voltage is in percent of 10V. So a 25% reading indicates 2.5V measured
-        $adc_voltage_shelly = $shelly_api_device_response->{"input:100"}->percent;
-
-        $timestamp_unixtime = $shelly_api_device_response->sys->unixtime;
-
-        // get the timestamp of measurement from Shelly Plus 1 device
-        if ($timestamp_unixtime)
-        {
-          $timestamp_shellybm = (int) round( $timestamp_unixtime , 0);
-        }
-        else
-        {
-          $timestamp_shellybm = 0;
-        }
+        $adc_voltage_shelly = (float) $shelly_api_device_response->{"input:100"}->percent;
+        $timestamp_shellybm = (int)   $shelly_api_device_response->sys->unixtime;
 
         // calculate the current using the 65mV/A formula around 2.5V. Positive current is battery discharge
         $delta_voltage = $adc_voltage_shelly * 0.1 - 2.54;
